a96c8298f5a8154570a01607049824b7471e21c6
manreddy <ibra.manreddy@gmail.com>
2023-07-17 20:42:29 +0400

    removes js files

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,6 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-// script to create and download pdf
-// import { jsPDF } from "jspdf";
-// document.addEventListener("DOMContentLoaded", function() {
-//     const generateButton = document.getElementById("generateButton");
-//     generateButton.addEventListener("click", generatePDF);
-//   });
-  
-//   function generatePDF(event) {
-//     event.preventDefault(); // Prevents the default behavior of the link
-
-//     const doc = new jsPDF();
-//     const element = document.getElementById('resume');
-  
-//     doc.html(element, {
-//       callback: function (pdf) {
-//         const blob = pdf.output("blob");
-//         const link = document.createElement("a");
-//         link.href = URL.createObjectURL(blob);
-//         link.download = "generated_pdf.pdf";
-//         link.click();
-//       }
-//     });
-//   }
-
   document.addEventListener("DOMContentLoaded", function() {
     const downloadButton = document.getElementById("generateButton");
     downloadButton.addEventListener("click", generateAndDownloadPDF);
