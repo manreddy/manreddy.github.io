6eda45828b2425da20cae30ac0054780156b221a
manreddy <ibra.manreddy@gmail.com>
2023-07-17 21:20:07 +0400

    cleanup and modified projects

--- js/scripts.js
+++ js/scripts.js
@@ -6,51 +6,7 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     const downloadButton = document.getElementById("generateButton");
-//     downloadButton.addEventListener("click", generatePDF);
-//   });
-  
-//   function generatePDF() {
-//     const section = document.getElementById("resume");
-  
-//     // Create a new jsPDF instance
-//     const doc = new jsPDF();
-  
-//     // Set the font size for the PDF content
-//     doc.setFontSize(12);
-  
-//     // Traverse the DOM section and add its content to the PDF
-//     traverseDOMSection(section, doc);
-  
-//     // Save the PDF file
-//     doc.save("generated_pdf.pdf");
-//   }
-  
-//   function traverseDOMSection(element, doc) {
-//     const children = element.childNodes;
-  
-//     for (let i = 0; i < children.length; i++) {
-//       const child = children[i];
-  
-//       if (child.nodeType === 1) {
-//         const tagName = child.tagName.toLowerCase();
-  
-//         // Add supported tags to the PDF content
-//         if (tagName === "p" || tagName === "h1" || tagName === "h2" || tagName === "h3") {
-//           doc.text(child.textContent, 10, doc.autoTable.previous.finalY + 10);
-//         } else if (tagName === "img") {
-//           // Handle images separately if needed
-//           // You can add images to the PDF using 'doc.addImage'
-//         } else {
-//           // Recursively traverse child elements
-//           traverseDOMSection(child, doc);
-//         }
-//       }
-//     }
-//   }
-  
-
+// code to change html to pdf
   document.addEventListener("DOMContentLoaded", function() {
     const downloadButton = document.getElementById("generateButton");
     downloadButton.addEventListener("click", generatePDF);
@@ -61,9 +17,9 @@
   
     html2pdf()
       .set({
-        filename: "generated_pdf.pdf",
+        filename: "IbrahimResume.pdf",
         margin: 10,
-        image: { type: "jpeg", quality: 0.98 },
+        image: { type: "jpeg", quality: 1 },
         html2canvas: { scale: 2 },
         jsPDF: { unit: "mm", format: "a4", orientation: "portrait" }
       })
