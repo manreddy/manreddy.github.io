1079787217b1806c1e1972c32a68738860b4d7c4
manreddy <ibra.manreddy@gmail.com>
2023-07-17 21:08:08 +0400

    use html 2pdf

--- js/scripts.js
+++ js/scripts.js
@@ -6,47 +6,68 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
+//     const downloadButton = document.getElementById("generateButton");
+//     downloadButton.addEventListener("click", generatePDF);
+//   });
+  
+//   function generatePDF() {
+//     const section = document.getElementById("resume");
+  
+//     // Create a new jsPDF instance
+//     const doc = new jsPDF();
+  
+//     // Set the font size for the PDF content
+//     doc.setFontSize(12);
+  
+//     // Traverse the DOM section and add its content to the PDF
+//     traverseDOMSection(section, doc);
+  
+//     // Save the PDF file
+//     doc.save("generated_pdf.pdf");
+//   }
+  
+//   function traverseDOMSection(element, doc) {
+//     const children = element.childNodes;
+  
+//     for (let i = 0; i < children.length; i++) {
+//       const child = children[i];
+  
+//       if (child.nodeType === 1) {
+//         const tagName = child.tagName.toLowerCase();
+  
+//         // Add supported tags to the PDF content
+//         if (tagName === "p" || tagName === "h1" || tagName === "h2" || tagName === "h3") {
+//           doc.text(child.textContent, 10, doc.autoTable.previous.finalY + 10);
+//         } else if (tagName === "img") {
+//           // Handle images separately if needed
+//           // You can add images to the PDF using 'doc.addImage'
+//         } else {
+//           // Recursively traverse child elements
+//           traverseDOMSection(child, doc);
+//         }
+//       }
+//     }
+//   }
+  
+
+  document.addEventListener("DOMContentLoaded", function() {
     const downloadButton = document.getElementById("generateButton");
     downloadButton.addEventListener("click", generatePDF);
   });
   
   function generatePDF() {
-    const section = document.getElementById("resume");
+    const element = document.getElementById("resume");
   
-    // Create a new jsPDF instance
-    const doc = new jsPDF();
-  
-    // Set the font size for the PDF content
-    doc.setFontSize(12);
-  
-    // Traverse the DOM section and add its content to the PDF
-    traverseDOMSection(section, doc);
-  
-    // Save the PDF file
-    doc.save("generated_pdf.pdf");
-  }
-  
-  function traverseDOMSection(element, doc) {
-    const children = element.childNodes;
-  
-    for (let i = 0; i < children.length; i++) {
-      const child = children[i];
-  
-      if (child.nodeType === 1) {
-        const tagName = child.tagName.toLowerCase();
-  
-        // Add supported tags to the PDF content
-        if (tagName === "p" || tagName === "h1" || tagName === "h2" || tagName === "h3") {
-          doc.text(child.textContent, 10, doc.autoTable.previous.finalY + 10);
-        } else if (tagName === "img") {
-          // Handle images separately if needed
-          // You can add images to the PDF using 'doc.addImage'
-        } else {
-          // Recursively traverse child elements
-          traverseDOMSection(child, doc);
-        }
-      }
-    }
+    html2pdf()
+      .set({
+        filename: "generated_pdf.pdf",
+        margin: 10,
+        image: { type: "jpeg", quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "mm", format: "a4", orientation: "portrait" }
+      })
+      .from(element)
+      .save();
   }
   
