cbed709597287aa11b69d9297f55dfb217ce7df0
manreddy <ibra.manreddy@gmail.com>
2023-07-18 07:18:50 +0400

    removed options

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,10 @@
     html2pdf()
       .set({
         filename: "IbrahimResume.pdf",
-        html2canvas: { allowTaint: true,scale: 2, logging:true },
-        jsPDF: { unit: "mm", format: "a4", orientation: "portrait",zoom: 2}
+        // margin: 10,
+        // image: { type: "jpeg", quality: 0.98 },
+        // html2canvas: { allowTaint: true,scale: 2, logging:true },
+        // jsPDF: { unit: "mm", format: "a4", orientation: "portrait",zoom: 2}
       })
       .from(element)
       .save();
