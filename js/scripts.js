be9c3a08a38e0525d83e9ee53f34930ff347eec9
manreddy <ibra.manreddy@gmail.com>
2023-07-20 18:50:13 +0400

    cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
+// create variables
 const name = "Ibrahim Manreddy";
 const phoneNumber = "+230 58247847";
 const address = "Old Grand Port Road, Quartier Militaire, Mauritius";
@@ -48,41 +41,32 @@ document.addEventListener("DOMContentLoaded", function() {
   downloadButton.addEventListener("click", generatePDF);
 });
 
-// -------------------------------------------------------------------------
 function generatePDF() {
-// -------------------------------------------------------------------------
   // get resume section by id
   const resumeElement = document.getElementById("resume");
-  // resumeElement.style.margin = "0";
-  // resumeElement.style.padding = "0";
-  // resumeElement.style = "";
 
   // get the generate button element, we need it to remove the download button
   const generateButtonElement = resumeElement.querySelector("#generateButton");
 
   if (resumeElement && generateButtonElement) {
-
     // Clone the resumeSection element
     const clonedResumeSection = resumeElement.cloneNode(true);
 
     // Remove the generateButtonElement (download button) from the cloned resumeSection
     let clonedGenerateButtonElement = clonedResumeSection.querySelector("#generateButton");
-    // clonedGenerateButtonElement.style = "";
 
     if (clonedGenerateButtonElement) {
       clonedGenerateButtonElement.remove();
     }
 
     const htmlContent = clonedResumeSection ? clonedResumeSection.innerHTML : "";
-
     const newElement = `<!DOCTYPE html>
-    <html>
-    <head>
-      <title>PDF Template</title>
+<html>
+  <head>
+    <title>PDF Template</title>
       <link rel="stylesheet" href="css/styles.css">
-    </head>
-    <body>  
-
+  </head>
+  <body>  
     <div class="text-center">
       <h1 class="display-5 fw-bolder mb-0">
         <span>
@@ -91,42 +75,39 @@ function generatePDF() {
       </h1>
     </div> 
     <div class="d-flex align-items-center justify-content-between mb-2">
-                                <h2 class="text-primary fw-bolder mb-0">Personal</h2>
-                            </div>
-                            <div class="card shadow border-0 rounded-4 mb-auto">
-                                <div class="card-body p-3">
-                                    <div class="row align-items-center gx-4">
-                                        <div class="col text-center text-lg-start mb-2 mb-lg-0">
-                                            <div class="bg-light p-2 rounded-4">
-                                                <div class="text-primary fw-bolder"><h5>`+ name +`</h5></div>
-                                                <div class="small fw-bolder">DevOps Engineer</div>
-                                                <div class="small">`+ address +`</div>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-8">
-                                            <div class="mb-1">`+ email +`</div>
-                                            <div class="mb-1">`+ phoneNumber +`</div>
-                                            <div class="mb-1">`+ github +`</div>
-                                            <div class="mb-1">`+ linkedin +`</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+      <h2 class="text-primary fw-bolder mb-0">Personal</h2>
+    </div>
+      <div class="card shadow border-0 rounded-4 mb-auto">
+          <div class="card-body p-3">
+              <div class="row align-items-center gx-4">
+                  <div class="col text-center text-lg-start mb-2 mb-lg-0">
+                      <div class="bg-light p-2 rounded-4">
+                          <div class="text-primary fw-bolder"><h5>`+ name +`</h5></div>
+                          <div class="small fw-bolder">DevOps Engineer</div>
+                          <div class="small">`+ address +`</div>
+                      </div>
+                  </div>
+                  <div class="col-lg-8">
+                      <div class="mb-1">`+ email +`</div>
+                      <div class="mb-1">`+ phoneNumber +`</div>
+                      <div class="mb-1">`+ github +`</div>
+                      <div class="mb-1">`+ linkedin +`</div>
+                  </div>
+              </div>
+          </div>
+      </div>
     <div>
     ` 
     + htmlContent +
     `
     </div>
-    </body>
-    </html>`
-    // console.log(newElement)
-
+  </body>
+</html>`
     // Generate PDF from the modified clonedResumeSection
     html2pdf()
     .set({
       filename: "IbrahimResume.pdf",
       pagebreak: { mode: ['css'] },
-      // image: { type: 'jpeg', quality: 0.98 },
       margin: [5,8,0,8]
     })
     .from(newElement)
